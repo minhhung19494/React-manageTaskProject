ba731f3236506725e6308c96262754a6c2942132
minhhung <minhhung19494@gmail.com>
2019-11-25 12:34:21 +0700

    Updates

---
+++ precache-manifest.8438889ef4fb3a9a313b9d22e104f475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdce4c48d50753a37d1efdb6b305d27",
+    "url": "/React-manageTaskProject/index.html"
+  },
+  {
+    "revision": "5a4de88ddf7395a908c4",
+    "url": "/React-manageTaskProject/static/css/main.e1c64649.chunk.css"
+  },
+  {
+    "revision": "f3eb2c443a38d3709c3e",
+    "url": "/React-manageTaskProject/static/js/2.ea8bb2b3.chunk.js"
+  },
+  {
+    "revision": "5a4de88ddf7395a908c4",
+    "url": "/React-manageTaskProject/static/js/main.5f043c2e.chunk.js"
+  },
+  {
+    "revision": "da4809d2c94b1c09b293",
+    "url": "/React-manageTaskProject/static/js/runtime-main.5891f8ee.js"
+  }
+]);
